2c0f84fa0cedfce4f23db7630a316bc09034db97
ngohoanglan2k3 <ngohoanglan2k3@gmail.com>
2024-04-20 15:00:51 +0700

    delete

--- typesciptdemo/javascript/interface.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let emp = { name: 'NGL', age: 12, email: 'fdve@fsd.com', salary: 12000 };
-let [user1, user2] = [
-    { name: 'IGL', age: 19, email: '' },
-    { name: 'GLI', age: 19, email: '' },
-    { name: 'DFA', age: 19, email: '' }
-];
