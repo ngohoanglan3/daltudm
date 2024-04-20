bfd59c717881c8f179780fb88f75cc1b8bc13938
ngohoanglan2k3 <ngohoanglan2k3@gmail.com>
2024-04-20 11:52:46 +0700

    115220042024

--- typesciptdemo/class.js
+++ typesciptdemo/class.js
@@ -1,16 +1,38 @@
 "use strict";
 class Employee {
+    get id() {
+        return this._id;
+    }
+    set id(value) {
+        this._id = value;
+    }
+    get add() {
+        return this._add;
+    }
+    set add(value) {
+        this._add = value;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(value) {
+        this._name = value;
+    }
     constructor(id, add, name) {
         this.id = id;
         this.add = add;
         this.name = name;
     }
+    getNameAndAdd() {
+        return `${this.name} at ${this.add}`;
+    }
 }
 let emp_array = [];
-let emp1 = { id: 12, add: 'HN', name: 'NHL' };
+let emp1 = new Employee(12, 'HN', 'NHL');
 emp_array.push(emp1);
-let emp2 = { id: 2213, add: 'BG', name: 'NGR' };
+let emp2 = new Employee(2213, 'BG', 'NGR');
 emp_array.push(emp2);
 for (let i in emp_array) {
     console.log(emp_array[i]);
 }
+console.log(emp_array[0].getNameAndAdd());
--- typesciptdemo/class.ts
+++ typesciptdemo/class.ts
@@ -1,12 +1,34 @@
 class Employee {
-    id!: number
-    add!: string
-    name!: string
+    private _id!: number
+    public get id(): number {
+        return this._id
+    }
+    public set id(value: number) {
+        this._id = value
+    }
+    private _add!: string
+    public get add(): string {
+        return this._add
+    }
+    public set add(value: string) {
+        this._add = value
+    }
+    private _name!: string
+    public get name(): string {
+        return this._name
+    }
+    public set name(value: string) {
+        this._name = value
+    }
 
     constructor(id: number, add: string, name: string) {
         this.id = id
         this.add = add
         this.name = name
+    }
+
+    getNameAndAdd(): String {
+        return `${this.name} at ${this.add}`
     }
 
 }
@@ -15,11 +37,12 @@ type emp<T> = T[]
 
 let emp_array: emp<Employee> = []
 
-let emp1:Employee = {id: 12, add: 'HN', name: 'NHL'}
+let emp1 = new Employee(12, 'HN', 'NHL')
 emp_array.push(emp1)
 
-let emp2:Employee = {id:2213, add: 'BG', name: 'NGR'}
+let emp2 = new Employee(2213, 'BG', 'NGR')
 emp_array.push(emp2)
-for(let i in emp_array) {
+for (let i in emp_array) {
     console.log(emp_array[i])
 }
+console.log(emp_array[0].getNameAndAdd())
