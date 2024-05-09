2178c5d423e46beb5a5d94d2f5b523acb83f7ac1
ngohoanglan2k3 <ngohoanglan2k3@gmail.com>
2024-05-09 22:02:59 +0700

    test thử

---
+++ demo/src/app/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  form!: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  get f() {
+    return this.form.controls;
+  }
+
+  OnSubmit() {}
+}
