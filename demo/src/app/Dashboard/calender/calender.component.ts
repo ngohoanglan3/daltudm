52417fbc53be14fa2473df82338d8bdc13e70f72
ngohoanglan2k3 <ngohoanglan2k3@gmail.com>
2024-05-27 11:03:17 +0700

    up tạm

--- demo/src/app/Dashboard/calender/calender.component.ts
+++ demo/src/app/Dashboard/calender/calender.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatCardModule } from '@angular/material/card';
-import { provideNativeDateAdapter } from '@angular/material/core';
+import { MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
 import { StudentDashboardComponent } from '../student-dashboard/student-dashboard.component';
 
 @Component({
@@ -9,7 +9,7 @@ import { StudentDashboardComponent } from '../student-dashboard/student-dashboar
   standalone: true,
   templateUrl: './calender.component.html',
   styleUrl: './calender.component.css',
-  providers: [provideNativeDateAdapter()],
+  providers: [provideNativeDateAdapter(), {provide: MAT_DATE_LOCALE, useValue: 'vi-VN'}],
   imports: [StudentDashboardComponent, MatCardModule, MatDatepickerModule],
 })
 export class CalenderComponent {
---
+++ demo/src/app/Dashboard/teacher-dashboard/teacher-dashboard.component.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-dashboard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './teacher-dashboard.component.html',
+  styleUrl: './teacher-dashboard.component.css',
+})
+export class TeacherDashboardComponent {
+  showBar = false;
+  open = false;
+
+  toggleBar() {
+    this.showBar = !this.showBar;
+  }
+
+  closeBar() {
+    this.showBar = false;
+  }
+
+  openBar() {
+    this.open = !this.open;
+  }
+
+  hide() {
+    this.open = false;
+  }
+}
